d6a815aa617fcc8fc15f2701dd81b9134567612b
EpicWolverine <epicwolverine@me.com>
2016-12-23 15:26:42 -0500

    Fixed mappings and fleshed out controller test

--- examples/test_package/src/listener.cpp
+++ examples/test_package/src/listener.cpp
@@ -7,7 +7,7 @@
  */
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard:%s", msg->data.c_str());
 }
 
 int main(int argc, char **argv)
