fd181ec118ea30438eaf2ab837afbf67188350bd
EpicWolverine <epicwolverine@me.com>
2016-12-29 16:40:13 -0500

    Set up waypoint service

--- yeti_snowplow/src/waypoint_selection.cpp
+++ yeti_snowplow/src/waypoint_selection.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "yeti_snowplow/waypoint.h"
 
 #include <fstream>
 #include <iostream>
@@ -8,6 +9,8 @@
 using namespace std;
 
 #include "containers/Target.h"
+
+vector<Target> targetLocationList;
 
 //because C++ doesn't have built in string splitting http://stackoverflow.com/a/236803
 void split(const std::string &s, char delim, std::vector<std::string> &elems) {
@@ -48,6 +51,24 @@ vector<Target> ReadFile(string filename){
 	return navigationPoints;
 }
 
+bool waypoint(yeti_snowplow::waypoint::Request  &req,
+              yeti_snowplow::waypoint::Response &res){
+	ROS_INFO("Request: %i", req.ID);
+	if (req.ID > -1 && req.ID < targetLocationList.size()){
+		res.x = targetLocationList[req.ID].location.x;
+		res.y = targetLocationList[req.ID].location.y;
+		res.heading = targetLocationList[req.ID].location.heading;
+		res.dir = targetLocationList[req.ID].dir;
+		res.PID = targetLocationList[req.ID].PID;
+		res.speed = targetLocationList[req.ID].speed;
+		ROS_INFO("Response: x=%f y=%f heading=%f dir=%i PID=%s speed=%f", res.x, res.y, res.heading, res.dir, res.PID?"true":"false", res.speed);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 int main(int argc, char **argv){
 	ros::init(argc, argv, "waypoint_selection");
 
@@ -57,11 +78,13 @@ int main(int argc, char **argv){
 	if (ros::param::get("navigationFile", navigationFile)){
 		ROS_INFO("Using navigationFile %s", navigationFile.c_str());
 
-		vector<Target> targetLocationList = ReadFile(navigationFile);
+		targetLocationList = ReadFile(navigationFile);
 
 		for(int i = 0; i < targetLocationList.size(); i++){
 			targetLocationList[i].Print();
 		}
+
+		ros::ServiceServer service = n.advertiseService("waypoint", waypoint);
 
 		ros::spin();
 	}
