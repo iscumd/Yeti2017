7ef043219e65fcd841d7673e6bb0fdf3fcbbe4b4
Michael <mbowyer@umich.edu>
2017-01-10 23:22:18 -0500

    Added Localization node(incomplete) and landmark location txt files, updated Cmake and package.xml files

--- yeti_snowplow/src/scan_to_xy_node.cpp
+++ yeti_snowplow/src/scan_to_xy_node.cpp
@@ -1,27 +1,23 @@
 #include <ros/ros.h>
 
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/LaserScan.h>
 #include <laser_geometry/laser_geometry.h>
-// PCL specific includes
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 
-ros::Publisher pcl_pub;
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+ros::Publisher pub;
 laser_geometry::LaserProjection projector_;
 
 void scan_to_XY (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
   // Create a container for the data.
-  sensor_msgs::PointCloud2 cloud;
+  sensor_msgs::PointCloud cloud;
 
   //convert scan data into point cloud
   projector_.projectLaser(*scan_in, cloud);
 
  //publish point cloud
-  pcl_pub.publish(cloud);  
+  pub.publish(cloud);  
 
 }
 
@@ -37,7 +33,7 @@ int main (int argc, char** argv)
   scanSub = nh.subscribe("scan", 1, scan_to_XY);
 
   // Create a ROS publisher for the output point cloud
-  pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("pcl_output", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud>("laser_scan_point_cloud", 1);
 
   // Spin
   ros::spin ();
